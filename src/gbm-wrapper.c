b1a88b0da79a68d4d1b409a6ea8c174aefc6fd14
Matt Flax <flatmax@flatmax.org>
2019-11-08 13:40:28 +1100

    add dummy warnings for the gbm calls.

--- src/gbm-wrapper.c
+++ src/gbm-wrapper.c
@@ -9,34 +9,40 @@
 
 #include <gbm.h>
 #include <drm_fourcc.h>
+#include <stdio.h>
 
 union gbm_bo_handle
 gbm_bo_get_handle_for_plane (struct gbm_bo *bo, int plane)
 {
+    printf("WARNING : dummy call to gbm_bo_get_handle_for_plane\n");
     return gbm_bo_get_handle (bo);
 }
 
 uint64_t
 gbm_bo_get_modifier (struct gbm_bo *bo)
 {
+    printf("WARNING : dummy call to gbm_bo_get_modifier\n");
     return DRM_FORMAT_MOD_INVALID;
 }
 
 uint32_t
 gbm_bo_get_offset (struct gbm_bo *bo, int plane)
 {
+    printf("WARNING : dummy call to gbm_bo_get_offset\n");
     return 0;
 }
 
 int
 gbm_bo_get_plane_count (struct gbm_bo *bo)
 {
+    printf("WARNING : dummy call to gbm_bo_get_plane_count\n");
     return 1;
 }
 
 uint32_t
 gbm_bo_get_stride_for_plane (struct gbm_bo *bo, int plane)
 {
+    printf("WARNING : dummy call to gbm_bo_get_stride_for_plane\n");
     return gbm_bo_get_stride (bo);
 }
 
@@ -46,5 +52,6 @@ gbm_surface_create_with_modifiers (struct gbm_device *gbm, uint32_t width,
                                    const uint64_t *modifiers,
                                    const unsigned int count)
 {
+    printf("WARNING : dummy call to gbm_surface_create_with_modifiers\n");
     return gbm_surface_create (gbm, width, height, format, 0);
 }
